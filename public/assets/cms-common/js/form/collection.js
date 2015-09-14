5e323ecd4fba31b97543207d66ec14084122a409
Dmitry Popov <d.popov@altgraphic.com>
2015-09-14 17:58:19 +0300

    Initial commit

--- public/assets/cms-common/js/form/collection.js
+++ public/assets/cms-common/js/form/collection.js
@@ -7,35 +7,40 @@ CmsCommon.Form = {};
 ;(function($){
 
 	CmsCommon.Form.Collection = {
+		addFieldset: function(button, placement) {
+			var fieldset = $(button).closest("fieldset"),
+				template = $(fieldset).find(">span").data("template"),
+				minMax = this.minMaxCounter($(fieldset).find(">[data-counter]"));
 
-		addFieldset: function(button, placement)
-		{
-			if (!placement) {
-				placement = "append";
-			}
-			var fieldset = $(button).closest("fieldset");
-			var template = $(fieldset).find(">span").data("template");
-			if (placement === "append") {
-				var currentCount = $(fieldset).find(">fieldset").last().data("counter");
-			} else {
-				var currentCount = $(fieldset).find(">fieldset").first().data("counter");
-			}
-	        if (typeof currentCount === "undefined") {
-	        	currentCount = -1;
-	        }
-	        template = template.replace(/__index__|--index--/g, currentCount + 1);
-	        if (placement === "append") {
-	        	$(fieldset).append(template);
+	        template = template.replace(/__index__|--index--/g, minMax.max === null ? 0 : minMax.max + 1);
+	        if (!placement || placement === "append") {
+	        	fieldset.append(template);
 	        } else {
-	        	$(fieldset).prepend(template);
+	        	if (fieldset.find("[type=hidden]")) {
+	        		fieldset.find("[type=hidden]").after(template);
+	        	} else if (fieldset.find(">legend")) {
+	        		fieldset.find(">legend").after(template);
+	        	} else {
+	        		fieldset.prepend(template);
+	        	}
+	        	$(".selectpicker").selectpicker('refresh');
 	        }
 	        return false;
 		},
 
-		removeFieldset: function(button)
-		{
-			var fieldset = $(button).closest("fieldset");
-			$(fieldset).remove();
+		removeFieldset: function(button) {
+			$(button).closest("[data-counter]").remove();
+		},
+
+		minMaxCounter: function(selector)  {
+			var min = null, max = null;
+			$(selector).each(function() {
+				var id = parseInt($(this).data("counter"), 10);
+			    if ((min===null) || (id < min)) { min = id; }
+			    if ((max===null) || (id > max)) { max = id; }
+			});
+
+			return {min: min, max: max};
 		}
 	};
 
